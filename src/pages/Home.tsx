7a6fa62a86421e7c048a5de7459af298661c83ad
madweed95 <drwho.spam@gmail.com>
2022-06-28 16:28:15 +0200

    add infinite scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import {
+  IonButton,
   IonContent,
   IonHeader,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
   IonItem,
   IonLabel,
   IonList,
@@ -10,11 +13,13 @@ import {
   IonSpinner,
   IonTitle,
   IonToolbar,
+  useIonViewWillEnter,
 } from "@ionic/react";
 import React, { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import "./Css/Style.css";
+import { push } from "ionicons/icons";
 
 export interface Hero {
   name: string;
@@ -24,24 +29,49 @@ export interface Hero {
   films: string[];
   starships: string[];
 }
+export interface Hero2 {
+  name: string;
+  height: string;
+  mass: string;
+  gender: string;
+  films: string[];
+  starships: string[];
+}
 
 const Home: React.FC = () => {
-  let history = useHistory();
+  const [isInfiniteDisabled, setInfiniteDisabled] = useState(false);
   const [heroes, setHeroes] = useState<Hero[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  let history = useHistory();
+
+  const pushData = () => {
+    const URL = "https://swapi.dev/api/people/?page=2";
+    return axios({
+      url: URL,
+      method: "get",
+    }).then((res) => {
+      // console.log(res.data.results);
+      console.log(res.data.results);
+      setHeroes([...heroes, ...res.data.results]);
+    });
+  };
+  const loadData = (ev: any) => {
+    setTimeout(() => {
+      pushData();
+      console.log("Loaded data");
+      ev.target.complete();
+      if (heroes.length === 1000) {
+        setInfiniteDisabled(true);
+      }
+    }, 500);
+  };
 
   React.useEffect(() => {
     fetchHeroes();
   }, []);
 
-  const refresh = (e: CustomEvent) => {
-    setTimeout(() => {
-      e.detail.complete();
-    }, 3000);
-  };
-
   const URL = "https://swapi.dev/api/people";
-  const fetchHeroes = async () => {
+  const fetchHeroes = () => {
     return axios({
       url: URL,
       method: "get",
@@ -55,26 +85,19 @@ const Home: React.FC = () => {
   }
 
   return (
-    <IonPage id="home-page">
+    <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Star Wars Heroes</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonRefresher slot="fixed" onIonRefresh={refresh}>
-          <IonRefresherContent></IonRefresherContent>
-        </IonRefresher>
+        <IonHeader collapse="condense"></IonHeader>
 
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Inbox</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <IonList>
-          {heroes.map((h) => (
+          {heroes.map((h, i) => (
             <IonItem
-              key={h.name}
+              key={i}
               onClick={() => {
                 history.push(`/hero/${h.name}`, { params: h });
               }}
@@ -83,7 +106,25 @@ const Home: React.FC = () => {
               <IonLabel className="ion-text-wrap">{h.name}</IonLabel>
             </IonItem>
           ))}
+          {/* {data.map((item, index) => {
+            return (
+              <IonItem key={index}>
+                <IonLabel>{item}</IonLabel>
+              </IonItem>
+            );
+          })} */}
         </IonList>
+
+        <IonInfiniteScroll
+          onIonInfinite={loadData}
+          threshold="100px"
+          disabled={isInfiniteDisabled}
+        >
+          <IonInfiniteScrollContent
+            loadingSpinner="crescent"
+            loadingText="Loading more heroes..."
+          ></IonInfiniteScrollContent>
+        </IonInfiniteScroll>
       </IonContent>
     </IonPage>
   );
